b615a13d0000821eb54c8425f14ac1ae22959ea4
玄冰 <1300272284@qq.com>
2024-11-29 19:32:25 +0800

    🐛 fix: 修复获取值可能为空

--- utools/preload.ts
+++ utools/preload.ts
@@ -2,9 +2,9 @@
  * @description 系统版本信息
  */
 window.versions = {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
+  node: () => process.versions.node || '',
+  chrome: () => process.versions.chrome || '',
+  electron: () => process.versions.electron || '',
 };
 
 /**
